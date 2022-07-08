bf190c4886158f54743e244fdb0305372fd48f4d
Aron Adams <aronrapetto@hotmail.com>
2022-07-08 17:06:20 -0300

    elementos renderizados, requisito 1 concluido

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
+import ProviderPlanetsContext from './context/ProviderPlanetsContext';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <ProviderPlanetsContext>
+      <Table />
+    </ProviderPlanetsContext>
   );
 }
 
