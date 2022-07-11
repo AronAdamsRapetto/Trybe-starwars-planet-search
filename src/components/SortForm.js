422e1e5001b06001c395efcd8d5fab698590c418
Aron Adams <aronrapetto@hotmail.com>
2022-07-11 02:33:02 -0300

    componente para adicionar filtragem criado

---
+++ src/components/SortForm.js
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react';
+import planetsContext from '../context/planetsContext';
+
+function SortForm() {
+  const { columnFilters, order, setOrder, setIsOrdered } = useContext(planetsContext);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setOrder((oldState) => ({
+      ...oldState,
+      [name]: value,
+    }));
+  };
+
+  const handleClick = () => {
+    setIsOrdered(true);
+  };
+
+  return (
+    <section>
+      <h3>Ordem</h3>
+      <select
+        data-testid="column-sort"
+        name="column"
+        onChange={ handleChange }
+        value={ order.column }
+      >
+        {
+          columnFilters.map((filter) => (
+            <option key={ filter } value={ filter }>{ filter }</option>
+          ))
+        }
+      </select>
+      <label htmlFor="asc-radio">
+        Ascendente
+        <input
+          type="radio"
+          name="sort"
+          id="asc-radio"
+          data-testid="column-sort-input-asc"
+          value="ASC"
+          onChange={ handleChange }
+          checked={ order.sort === 'ASC' }
+        />
+      </label>
+      <label htmlFor="desc-radio">
+        Descendente
+        <input
+          type="radio"
+          name="sort"
+          id="desc-radio"
+          data-testid="column-sort-input-desc"
+          value="DESC"
+          onChange={ handleChange }
+          checked={ order.sort === 'DESC' }
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="column-sort-button"
+        onClick={ handleClick }
+      >
+        Ordenar
+      </button>
+    </section>
+  );
+}
+
+export default SortForm;
